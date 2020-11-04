13a4213d3ddb94c6822cfb1cf506d7d6dc11d0de
himar33 <himar33@users.noreply.github.com>
2020-11-04 18:58:53 +0100

    .

---
+++ Handout 4/ModuleSceneIntro.h
@@ -0,0 +1,37 @@
+#pragma once
+#include "Module.h"
+#include "p2List.h"
+#include "p2Point.h"
+#include "Globals.h"
+#include "Animation.h"
+
+class PhysBody;
+
+class ModuleSceneIntro : public Module
+{
+public:
+	ModuleSceneIntro(Application* app, bool start_enabled = true);
+	~ModuleSceneIntro();
+
+	bool Start();
+	update_status Update();
+	bool CleanUp();
+	void OnCollision(PhysBody* bodyA, PhysBody* bodyB);
+
+public:
+	p2List<PhysBody*> circles;
+	p2List<PhysBody*> boxes;
+	p2List<PhysBody*> ricks;
+
+	PhysBody* sensor;
+	bool sensed;
+
+	Animation* currentAnim = nullptr;
+	Animation bgAnim;
+	SDL_Texture* bg;
+	SDL_Texture* circle;
+
+	uint bonus_fx;
+	p2Point<int> ray;
+	bool ray_on;
+};
