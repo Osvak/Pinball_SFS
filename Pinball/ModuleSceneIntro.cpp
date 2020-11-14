9963aa67fd4d1f01594c6a0ffee1404fd14235e8
himar33 <himar33@users.noreply.github.com>
2020-11-14 22:39:26 +0100

    Game Quite done

--- Pinball/ModuleSceneIntro.cpp
+++ Pinball/ModuleSceneIntro.cpp
@@ -6,6 +6,8 @@
 #include "ModuleTextures.h"
 #include "ModuleAudio.h"
 #include "ModulePhysics.h"
+#include "ModuleFont.h"
+#include <stdio.h>
 
 ModuleSceneIntro::ModuleSceneIntro(Application* app, bool start_enabled) : Module(app, start_enabled)
 {
@@ -28,6 +30,9 @@ bool ModuleSceneIntro::Start()
 	flipperSound = App->audio->LoadFx("pinball/flipper.wav");
 	kickerSound = App->audio->LoadFx("pinball/kicker.wav");
 	deathSound = App->audio->LoadFx("pinball/death.wav");
+	bumperSound = App->audio->LoadFx("pinball/bumper.wav");
+	skySound = App->audio->LoadFx("pinball/sky.wav");
+	multiSound = App->audio->LoadFx("pinball/multi.wav");
 
 	circle = App->textures->Load("pinball/ball.png"); 
 	bg = App->textures->Load("pinball/background.png");
@@ -36,6 +41,9 @@ bool ModuleSceneIntro::Start()
 	longTube = App->textures->Load("pinball/tube.png");
 	leftSquare = App->textures->Load("pinball/side.png");
 	rightSquare = App->textures->Load("pinball/side2.png");
+	bumperTex = App->textures->Load("pinball/bumper.png");
+	skyTex = App->textures->Load("pinball/sky.png");
+	multiTex = App->textures->Load("pinball/multipliers.png");
 
 	int walls[154] = {
 		437, 832,
@@ -303,6 +311,15 @@ bool ModuleSceneIntro::Start()
 
 	dead = App->physics->CreateRectangleSensor(245, 950, 481, 2);
 	layer1 = App->physics->CreateRectangleSensor(195, 396, 15, 15);
+	startPoint = App->physics->CreateRectangleSensor(450, 802, 15, 15);
+
+	pointS = App->physics->CreateRectangleSensor(325, 140, 15, 15);
+	pointK = App->physics->CreateRectangleSensor(368, 140, 15, 15);
+	pointY = App->physics->CreateRectangleSensor(410, 140, 15, 15);
+
+	bumper1 = App->physics->CreateCircle(139, 190, 22, b2_staticBody);
+	bumper2 = App->physics->CreateCircle(120, 250, 22, b2_staticBody);
+	bumper3 = App->physics->CreateCircle(190, 250, 22, b2_staticBody);
 
 	createBall = true;
 	up = false;
@@ -322,10 +339,12 @@ bool ModuleSceneIntro::CleanUp()
 // Update: draw background
 update_status ModuleSceneIntro::Update()
 {
+	//1RST LAYER
 	App->renderer->Blit(bg, 0, 0, NULL, 1.0f);
 
 	if (createBall == true)
 	{
+		multi = 1;
 		circles.add(App->physics->CreateCircle(441, 802, 12));
 		circles.getLast()->data->listener = this;
 		createBall = false;
@@ -342,63 +361,139 @@ update_status ModuleSceneIntro::Update()
 
 	if(App->input->GetKey(SDL_SCANCODE_LEFT) == KEY_REPEAT)
 	{
-		App->physics->leftFlipper->body->ApplyForce({ 10, 80 }, { 0, 0 }, true);
+		App->physics->leftFlipper->body->ApplyForce({ 10, 70 }, { 0, 0 }, true);
 
 		if (App->input->GetKey(SDL_SCANCODE_LEFT) == KEY_UP)
 		{
-			App->physics->leftFlipper->body->ApplyForce({ -10, -80 }, { 0, 0 }, true);
+			App->physics->leftFlipper->body->ApplyForce({ -10, -70 }, { 0, 0 }, true);
 		}
 
-		App->physics->leftFlipper2->body->ApplyForce({ 10, 80 }, { 0, 0 }, true);
+		App->physics->leftFlipper2->body->ApplyForce({ 10, 70 }, { 0, 0 }, true);
 
 		if (App->input->GetKey(SDL_SCANCODE_LEFT) == KEY_UP)
 		{
-			App->physics->leftFlipper2->body->ApplyForce({ -10, -80 }, { 0, 0 }, true);
+			App->physics->leftFlipper2->body->ApplyForce({ -10, -70 }, { 0, 0 }, true);
 		}
 	}
 
 	if (App->input->GetKey(SDL_SCANCODE_RIGHT) == KEY_REPEAT)
 	{
-		App->physics->rightFlipper->body->ApplyForce({ -10, -80 }, { 0, 0 }, true);
+		App->physics->rightFlipper->body->ApplyForce({ -10, -50 }, { 0, 0 }, true);
 
 		if (App->input->GetKey(SDL_SCANCODE_RIGHT) == KEY_UP)
 		{
-			App->physics->rightFlipper->body->ApplyForce({ 10, 80 }, { 0, 0 }, true);
+			App->physics->rightFlipper->body->ApplyForce({ 10, 70 }, { 0, 0 }, true);
 		}
 
-		App->physics->rightFlipper2->body->ApplyForce({ -10, -80 }, { 0, 0 }, true);
+		App->physics->rightFlipper2->body->ApplyForce({ -10, -70 }, { 0, 0 }, true);
 
 		if (App->input->GetKey(SDL_SCANCODE_RIGHT) == KEY_UP)
 		{
-			App->physics->rightFlipper2->body->ApplyForce({ 10, 80 }, { 0, 0 }, true);
+			App->physics->rightFlipper2->body->ApplyForce({ 10, 70 }, { 0, 0 }, true);
 		}
 	}
 
 	int x, y;
 	App->physics->leftFlipper->GetPosition(x, y);
-	b2Vec2 point = { (float)x, (float)y };
 	App->renderer->Blit(leftFlipperTex, x, y, NULL, 1.0f, App->physics->leftFlipper->body->GetAngle() * RADTODEG, PIXEL_TO_METERS(12), PIXEL_TO_METERS(12));
 
 	App->physics->rightFlipper->GetPosition(x, y);
-	//point = { (float)x, (float)y };
 	App->renderer->Blit(rightFlipperTex, x, y, NULL, 1.0f, App->physics->rightFlipper->body->GetAngle() * RADTODEG, PIXEL_TO_METERS(60), PIXEL_TO_METERS(12));
 
 	App->physics->leftFlipper2->GetPosition(x, y);
-	//point = { (float)x, (float)y };
 	App->renderer->Blit(leftFlipperTex, x, y, NULL, 1.0f, App->physics->leftFlipper2->body->GetAngle() * RADTODEG, PIXEL_TO_METERS(12), PIXEL_TO_METERS(12));
 
 	App->physics->rightFlipper2->GetPosition(x, y);
-	//point = { (float)x, (float)y };
 	App->renderer->Blit(rightFlipperTex, x, y, NULL, 1.0f, App->physics->rightFlipper2->body->GetAngle() * RADTODEG, PIXEL_TO_METERS(60), PIXEL_TO_METERS(12));
-	
+
+	SDL_Rect noTouching = { 0, 0, 56, 53 };
+	SDL_Rect Touching = { 56, 0, 56, 53 };
+	if (touching1 == true)
+	{
+		bumper1->GetPosition(x, y);
+		App->renderer->Blit(bumperTex, x - 5, y - 3, &Touching, 1.0f);
+	}
+	else
+	{
+		bumper1->GetPosition(x, y);
+		App->renderer->Blit(bumperTex, x - 5, y - 3, &noTouching, 1.0f);
+	}
+
+	if (touching2 == true)
+	{
+		bumper2->GetPosition(x, y);
+		App->renderer->Blit(bumperTex, x - 5, y - 3, &Touching, 1.0f);
+	}
+	else
+	{
+		bumper2->GetPosition(x, y);
+		App->renderer->Blit(bumperTex, x - 5, y - 3, &noTouching, 1.0f);
+	}
+
+	if (touching3 == true)
+	{
+		bumper3->GetPosition(x, y);
+		App->renderer->Blit(bumperTex, x - 5, y - 3, &Touching, 1.0f);
+	}
+	else
+	{
+		bumper3->GetPosition(x, y);
+		App->renderer->Blit(bumperTex, x - 5, y - 3, &noTouching, 1.0f);
+	}
+
+	if (sCond == true)
+	{
+		App->renderer->Blit(skyTex, 319, 104, &sRect, 1.0f);
+	}
+	if (kCond == true)
+	{
+		App->renderer->Blit(skyTex, 354, 104, &kRect, 1.0f);
+	}
+	if (yCond == true)
+	{
+		App->renderer->Blit(skyTex, 394, 104, &yRect, 1.0f);
+	}
+
+	if (multi == 2)
+	{
+		App->renderer->Blit(multiTex, 150, 798, &x2, 1.0f);
+	}
+	else if (multi == 4)
+	{
+		App->renderer->Blit(multiTex, 150, 798, &x2, 1.0f);
+		App->renderer->Blit(multiTex, 188, 789, &x4, 1.0f);
+	}
+	else if (multi == 6)
+	{
+		App->renderer->Blit(multiTex, 150, 798, &x2, 1.0f);
+		App->renderer->Blit(multiTex, 188, 789, &x4, 1.0f);
+		App->renderer->Blit(multiTex, 220, 785, &x6, 1.0f);
+	}
+	else if (multi == 8)
+	{
+		App->renderer->Blit(multiTex, 150, 798, &x2, 1.0f);
+		App->renderer->Blit(multiTex, 188, 789, &x4, 1.0f);
+		App->renderer->Blit(multiTex, 220, 785, &x6, 1.0f);
+		App->renderer->Blit(multiTex, 250, 788, &x8, 1.0f);
+	}
+	else if (multi == 10)
+	{
+		App->renderer->Blit(multiTex, 150, 798, &x2, 1.0f);
+		App->renderer->Blit(multiTex, 188, 789, &x4, 1.0f);
+		App->renderer->Blit(multiTex, 220, 785, &x6, 1.0f);
+		App->renderer->Blit(multiTex, 250, 788, &x8, 1.0f);
+		App->renderer->Blit(multiTex, 285, 798, &x10, 1.0f);
+	}
+
 	p2List_item<PhysBody*>* c = circles.getFirst();
 
 	x, y;
 	b2Vec2 force = { 0.f, -2.f };
 	c->data->GetPosition(x, y);
-	point = { (float)x, (float)y };
+	b2Vec2 point = { (float)x, (float)y };
 	if (up == false)
 	{
+		//2ND LAYER
 		App->renderer->Blit(circle, x - 6, y - 6, NULL, 1.0f, c->data->GetRotation());
 	}
 	if ((App->input->GetKey(SDL_SCANCODE_DOWN) == KEY_DOWN) && start == true)
@@ -410,6 +505,7 @@ update_status ModuleSceneIntro::Update()
 	}
 	c->data->body->SetFixedRotation(false);
 
+	//3RD LAYER
 	App->renderer->Blit(longTube, 141, 51, NULL, 1.0f);
 	App->renderer->Blit(leftSquare, 54, 415, NULL, 1.0f);
 	App->renderer->Blit(rightSquare, 325, 451, NULL, 1.0f);
@@ -418,6 +514,31 @@ update_status ModuleSceneIntro::Update()
 	{
 		App->renderer->Blit(circle, x - 6, y - 6, NULL, 1.0f, c->data->GetRotation());
 	}
+	sprintf_s(scoreText, 10, "%d", score);
+	App->fonts->BlitText(10, 10, scoreFont, scoreText);
+
+	if (skyCount == 3)
+	{
+		App->audio->PlayFx(multiSound);
+		if (multi == 1)
+		{
+			multi += 1;
+		}
+		else
+		{
+			multi += 2;
+		}
+		sCond = false;
+		kCond = false;
+		yCond = false;
+		skyCount = 0;
+	}
+
+	return UPDATE_CONTINUE;
+}
+
+update_status ModuleSceneIntro::PostUpdate()
+{
 
 	return UPDATE_CONTINUE;
 }
@@ -452,6 +573,76 @@ void ModuleSceneIntro::OnCollision(PhysBody* bodyA, PhysBody* bodyB)
 
 	if (bodyA->body == leftBumper->body || bodyB->body == leftBumper->body)
 	{
-		
+		int x, y;
+		b2Vec2 force = { 0.5f, -0.5f };
+		circles.getFirst()->data->GetPosition(x, y);
+		b2Vec2 point = { (float)x, (float)y };
+		circles.getFirst()->data->body->ApplyForce(force, point, true);
+	}
+
+	if (bodyA->body == startPoint->body || bodyB->body == startPoint->body)
+	{
+		start = true;
+	}
+
+	if (bodyA->body == bumper1->body || bodyB->body == bumper1->body)
+	{
+		App->audio->PlayFx(bumperSound);
+		touching1 = true;
+		score += 100*multi;
+	}
+	else
+	{
+		touching1 = false;
+	}
+
+	if (bodyA->body == bumper2->body || bodyB->body == bumper2->body)
+	{
+		App->audio->PlayFx(bumperSound);
+		touching2 = true;
+		score += 100*multi;
+	}
+	else
+	{
+		touching2 = false;
+	}
+
+	if (bodyA->body == bumper3->body || bodyB->body == bumper3->body)
+	{
+		App->audio->PlayFx(bumperSound);
+		touching3 = true;
+		score += 100*multi;
+	}
+	else
+	{
+		touching3 = false;
+	}
+
+	if (bodyA->body == pointY->body || bodyB->body == pointY->body)
+	{
+		if (yCond != true && multi < 10)
+		{
+			App->audio->PlayFx(skySound);
+			yCond = true;
+			skyCount += 1;
+		}
+	}
+	if (bodyA->body == pointK->body || bodyB->body == pointK->body)
+	{
+		if (kCond != true && multi < 10)
+		{
+			App->audio->PlayFx(skySound);
+			kCond = true;
+			skyCount += 1;
+		}
+	}
+	if (bodyA->body == pointS->body || bodyB->body == pointS->body)
+	{
+		if (sCond != true && multi < 10)
+		{
+			App->audio->PlayFx(skySound);
+			sCond = true;
+			skyCount += 1;
+		}
 	}
 }
--- Pinball/ModuleSceneIntro.h
+++ Pinball/ModuleSceneIntro.h
@@ -14,6 +14,7 @@ public:
 
 	bool Start();
 	update_status Update();
+	update_status PostUpdate();
 	bool CleanUp();
 	void OnCollision(PhysBody* bodyA, PhysBody* bodyB);
 
@@ -21,19 +22,51 @@ public:
 	p2List<PhysBody*> circles;
 	PhysBody* dead;
 	PhysBody* layer1;
+	PhysBody* startPoint;
 	PhysBody* lt;
 	PhysBody* wall;
 	PhysBody* leftBumper;
 	PhysBody* rightBumper;
+
+	PhysBody* bumper1;
+	PhysBody* bumper2;
+	PhysBody* bumper3;
+
+	PhysBody* pointS;
+	PhysBody* pointK;
+	PhysBody* pointY;
+
 	bool createBall;
 	bool sensed;
 	bool up;
+	bool touching1 = false;
+	bool touching2 = false;
+	bool touching3 = false;
 
 	SDL_Texture* circle;
 	SDL_Texture* bg;
 	SDL_Texture* longTube;
 	SDL_Texture* leftSquare;
 	SDL_Texture* rightSquare;
+	SDL_Texture* bumperTex;
+	SDL_Texture* scoreTex;
+
+	SDL_Texture* skyTex;
+	SDL_Rect sRect = { 0,0,19,25 };
+	bool sCond = false;
+	SDL_Rect kRect = { 19,0,20,25 };
+	bool kCond = false;
+	SDL_Rect yRect = { 39,0,20,25 };
+	bool yCond = false;
+	int skyCount = 0;
+
+	SDL_Texture* multiTex;
+	SDL_Rect x2 = { 0,0,37,25 };
+	SDL_Rect x4 = { 37,0,36,25 };
+	SDL_Rect x6 = { 73,0,37,25 };
+	SDL_Rect x8 = { 109,0,36,25 };
+	SDL_Rect x10 = { 145,0,36,25 };
+	int multi = 1;
 
 	SDL_Texture* leftFlipperTex;
 	SDL_Texture* rightFlipperTex;
@@ -41,8 +74,15 @@ public:
 	uint flipperSound;
 	uint kickerSound;
 	uint deathSound;
+	uint bumperSound;
+	uint skySound;
+	uint multiSound;
 
 	p2Point<int> ray;
 	bool ray_on;
 	bool start;
+
+	int score = 0000;
+	int scoreFont = -1;
+	char scoreText[10] = { "\0" };
 };
